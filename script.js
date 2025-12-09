c3a837887397e0e21fd3538aab23d277825434ff
Mahdieh Taheri <maahiita20@gmail.com>
2025-12-09 12:08:24 +0330

    reveal section

--- script.js
+++ script.js
@@ -101,3 +101,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHight}px`,
 });
 headerObserver.observe(header);
+
+/*reveal section */
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
